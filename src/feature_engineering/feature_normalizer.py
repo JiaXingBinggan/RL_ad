1b4694103c6180115f4282a1e5c192b1396c6d96
JiaXingBingGAN <729869585@qq.com>
2018-12-06 21:46:29 +0800

    新增FM预测CTR(但还有bug

--- src/feature_engineering/feature_normalizer.py
+++ src/feature_engineering/feature_normalizer.py
@@ -35,3 +35,22 @@ print(data.head())
 
 data.to_csv('../../data/normalized_test_data.csv', index=False, header=False)
 
+train_data = pd.read_csv("../../data/train_encoder_data.csv", header=None)
+# 选择[ 1  2  4  6  7  8  9 10 12 13 15 16 20 21 26]号特征
+data = train_data.drop([0, 3, 5, 11, 14, 17, 18, 19, 22, 23, 24, 25], axis=1) # 去除这些特征
+data['clk'] = train_data[0]
+data['payprice'] = train_data[23]
+data['hour'] = train_data[2]
+print(data.head())
+data.to_csv('../../data/selected_train_data.csv', index=False, header=False)
+
+test_data = pd.read_csv("../../data/test_encoder_data.csv", header=None)
+# 选择[ 1  2  4  6  7  8  9 10 12 13 15 16 20 21 26]号特征
+data = test_data.drop([0, 3, 5, 11, 14, 17, 18, 19, 22, 23, 24, 25, 27, 28], axis=1) # 去除这些特征，同训练数据保持一致
+data['clk'] = test_data[0]
+data['payprice'] = test_data[23]
+data['hour'] = train_data[2]
+print(data.head())
+
+data.to_csv('../../data/seleted_test_data.csv', index=False, header=False)
+
