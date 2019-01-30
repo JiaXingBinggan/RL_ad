44cafc817b966e2f46443cb41abaaf2766ef4529
JiaXingBingGan <729869585@qq.com>
2019-01-30 14:17:12 +0800

    记录日期对应的weekday

--- src/feature_engineering/data_to_csv.py
+++ src/feature_engineering/data_to_csv.py
@@ -36,6 +36,15 @@ test_header = ['click', 'weekday', 'hour', 'bidid', 'timestamp', 'logtype', 'ipi
                'IP', 'region', 'city', 'adexchange', 'domain', 'url', 'urlid', 'slotid', 'slotwidth',
                'slotheight', 'slotvisibility', 'slotformat', 'slotprice', 'creative', 'bidprice',
                'payprice', 'keypage', 'advertiser', 'usertag', 'nclick', 'nconversation']
+'''
+0606 星期4
+0607 星期5
+0608 星期6
+0609 星期0
+0610 星期1
+0611 星期2
+0612 星期3
+'''
 # 选择一天的数据作为训练集和测试集
 train_data = pd.read_csv('../../data/train.csv')
 one_day_data = train_data[train_data.iloc[:, 1].isin([0,1,2,4,5,6])] # 选择特定值所在的行
