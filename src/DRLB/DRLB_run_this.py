1ed2c267c8320a36ac729acd7b650e10ef8d8bad
JiaXingBingGan <729869585@qq.com>
2019-04-22 11:26:06 +0800

    修改DRLB预算缩放问题

--- src/DRLB/DRLB_run_this.py
+++ src/DRLB/DRLB_run_this.py
@@ -140,7 +140,7 @@ def state_(budget, auc_num, auc_t_datas, auc_t_data_pctrs, lamda, B_t, time_t, r
     state_t = [time_t+1, B_t[time_t], ROL_t, BCR_t, CPM_t, WR_t, reward_t]
 
     net_reward_t = RewardNet.return_model_reward(state_t)
-    state_t = [(time_t + 1)/96, B_t[time_t]/5000000, ROL_t/96, BCR_t, CPM_t/100, WR_t, net_reward_t[0][0]]
+    state_t = [(time_t + 1)/96, B_t[time_t]/budget, ROL_t/96, BCR_t, CPM_t/100, WR_t, net_reward_t[0][0]]
     # state_t = [time_t + 1, B_t[time_t], ROL_t, BCR_t, CPM_t, WR_t, net_reward_t[0][0]]
 
     t_real_clks = np.sum(auc_t_datas.iloc[:, 0])
